fd2a7caea5c2e6c0e575b559c17eb00d389052a7
Igor Ciqueira <igor.silva@cbyk.com.br>
2022-06-02 10:45:27 -0300

    Adjust unit test

---
+++ app/src/test/java/com/igor/bestmovies/domain/usecase/GetPopularShowTest.kt
@@ -0,0 +1,37 @@
+package com.igor.bestmovies.domain.usecase
+
+import com.igor.bestmovies.data.repository.PopularShowRepository
+import com.igor.bestmovies.domain.model.Genre
+import com.igor.bestmovies.domain.model.Show
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class GetPopularShowTest {
+    private lateinit var interactor : GetPopularShow
+
+    @MockK
+    lateinit var repository: PopularShowRepository
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        interactor = GetPopularShow(repository)
+    }
+
+    @Test
+    fun `When return empty list`() = runBlockingTest{
+        val genre = mockk<List<Genre>>()
+        val shows = mockk<List<Show>>(relaxed = true)
+
+        coEvery { repository.fetchPopularShowsAsync() } returns mockk(relaxed = true)
+        val response = interactor(genre)
+
+        Assert.assertEquals(response.size, shows.size)
+    }
+}
---
+++ app/src/test/java/com/igor/bestmovies/domain/usecase/GetShowDetailTest.kt
@@ -0,0 +1,39 @@
+package com.igor.bestmovies.domain.usecase
+
+import com.igor.bestmovies.data.repository.ShowDetailRepository
+import com.igor.bestmovies.domain.model.Show
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class GetShowDetailTest {
+    private lateinit var interactor: GetShowDetail
+
+    @MockK
+    lateinit var repository: ShowDetailRepository
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        interactor = GetShowDetail(repository)
+    }
+
+    @Test
+    fun `Test when return show detail`() = runBlockingTest {
+        val show = mockk<Show>(relaxed = true)
+
+        coEvery { repository.fetchAsync(3) } returns mockk(relaxed = true)
+        val fetch = interactor(3)
+
+        every { show.id } returns 3
+        every { fetch?.id } returns 3
+
+        Assert.assertEquals(show.id, fetch?.id)
+    }
+}
